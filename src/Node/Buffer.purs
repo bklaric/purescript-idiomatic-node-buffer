8d6bd3b020431b56daeb869a5520152e428c9ec6
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-03-29 20:15:57 +0200

    Add concat and toString to Buffer

--- src/Node/Buffer.purs
+++ src/Node/Buffer.purs
@@ -7,6 +7,12 @@ module Node.Buffer
     , allocString_
     , fromString
     , fromString_
+    , concat
+    , concat_
+    , toString
+    , toString_
+    , toString__
+    , toString___
     ) where
 
 import Control.Monad.Effect (Effect)
@@ -42,3 +48,19 @@ fromString string encoding = fromStringImpl string (toNodeString encoding)
 
 fromString_ :: String -> Effect Buffer
 fromString_ string = fromStringImpl string undefined
+
+foreign import concat :: Array Buffer -> Int -> Effect Buffer
+
+concat_ :: Array Buffer -> Effect Buffer
+concat_ buffers = concat buffers undefined
+
+foreign import toString :: Encoding -> Int -> Int -> Buffer -> Effect String
+
+toString_ :: Encoding -> Int -> Buffer -> Effect String
+toString_ encoding start buffer = toString encoding start undefined buffer
+
+toString__ :: Encoding -> Buffer -> Effect String
+toString__ encoding buffer = toString_ encoding undefined buffer
+
+toString___ :: Buffer -> Effect String
+toString___ buffer = toString__ undefined buffer
